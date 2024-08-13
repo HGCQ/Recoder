47c21aff2705408ca76715eeaeafef4b6de80be7
Mangjun <myoungjun55@naver.com>
2024-08-13 12:56:39 +0900

    [+] 프로젝트 초기 설정

---
+++ server/src/main/java/yuhan/hgcq/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package yuhan.hgcq.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
