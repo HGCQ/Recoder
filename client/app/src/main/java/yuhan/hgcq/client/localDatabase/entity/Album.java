0dd8c41c722d901bf696b7843849fe68fea75eb7
LEEDDONG <ehdgus2580@naver.com>
2024-09-10 14:58:22 +0900

    [+] 로컬DB 수정 및 로컬 DB 테스트 완료

--- client/app/src/main/java/yuhan/hgcq/client/localDatabase/entity/Album.java
+++ client/app/src/main/java/yuhan/hgcq/client/localDatabase/entity/Album.java
@@ -3,16 +3,37 @@ package yuhan.hgcq.client.localDatabase.entity;
 import androidx.annotation.NonNull;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
+import androidx.room.TypeConverters;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import yuhan.hgcq.client.localDatabase.Converters;
+
+//import yuhan.hgcq.client.localDatabase.Converters;
 
 @Entity(tableName = "Album")
+@TypeConverters({Converters.class})
 public class Album {
 
+    public Album() {
+
+    }
+
+
+    // 정적 팩토리 메서드
+    public static Album create(@NonNull LocalDateTime date, @NonNull String name, @NonNull String content, @NonNull String region) {
+        Album album = new Album();
+        album.setDate(date);
+        album.setName(name);
+        album.setContent(content);
+        album.setRegion(region);
+        return album;
+    }
     @PrimaryKey(autoGenerate = true)
     private Long albumId;
     @NonNull
-    private LocalDate date;
+    private LocalDateTime date;
     @NonNull
     private String name;
     @NonNull
@@ -28,45 +49,49 @@ public class Album {
         this.albumId = albumId;
     }
 
-    public LocalDate getDate() {
+    @NonNull
+    public LocalDateTime getDate() {
         return date;
     }
 
-    public void setDate(LocalDate date) {
+    public void setDate(@NonNull LocalDateTime date) {
         this.date = date;
     }
 
+    @NonNull
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
+    public void setName(@NonNull String name) {
         this.name = name;
     }
 
+    @NonNull
     public String getContent() {
         return content;
     }
 
-    public void setContent(String content) {
+    public void setContent(@NonNull String content) {
         this.content = content;
     }
 
+    @NonNull
     public String getRegion() {
         return region;
     }
 
-    public void setRegion(String region) {
+    public void setRegion(@NonNull String region) {
         this.region = region;
     }
 
     @Override
     public String toString() {
         return "Album{" +
-                "album_id=" + albumId +
+                "albumId=" + albumId +
                 ", date=" + date +
-                ", outline='" + name + '\'' +
-                ", comment='" + content + '\'' +
+                ", name='" + name + '\'' +
+                ", content='" + content + '\'' +
                 ", region='" + region + '\'' +
                 '}';
     }
--- client/app/src/main/java/yuhan/hgcq/client/localDatabase/entity/Photo.java
+++ client/app/src/main/java/yuhan/hgcq/client/localDatabase/entity/Photo.java
@@ -3,10 +3,16 @@ package yuhan.hgcq.client.localDatabase.entity;
 import androidx.annotation.NonNull;
 import androidx.room.Entity;
 import androidx.room.ForeignKey;
+import androidx.room.Index;
 import androidx.room.PrimaryKey;
+import androidx.room.TypeConverters;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+
+import yuhan.hgcq.client.localDatabase.Converters;
+
+//import yuhan.hgcq.client.localDatabase.Converters;
 
 @Entity(tableName = "photo",
         foreignKeys = @ForeignKey(
@@ -14,11 +20,15 @@ import java.time.LocalDateTime;
                 parentColumns = "albumId",
                 childColumns = "albumId",
                 onDelete = ForeignKey.CASCADE
-        ))
+        ),
+        indices = {@Index(value = {"albumId"})})
+@TypeConverters({Converters.class})
 public class Photo {
+    public Photo() {
+    }
 
     @PrimaryKey(autoGenerate = true)
-    private int photoId;
+    private Long photoId;
     @NonNull
     private String name;
     @NonNull
@@ -32,73 +42,78 @@ public class Photo {
     @NonNull
     private LocalDateTime created;
 
-    // Getters and Setters
-    public int getPhoto_id() {
+    public Long getPhotoId() {
         return photoId;
     }
 
-    public void setPhoto_id(int photoId) {
+    public void setPhotoId(Long photoId) {
         this.photoId = photoId;
     }
 
+    @NonNull
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
+    public void setName(@NonNull String name) {
         this.name = name;
     }
 
+    @NonNull
     public String getPath() {
         return path;
     }
 
-    public void setPath(String path) {
+    public void setPath(@NonNull String path) {
         this.path = path;
     }
 
+    @NonNull
     public Long getAlbumId() {
         return albumId;
     }
 
-    public void setAlbumId(Long albumId) {
+    public void setAlbumId(@NonNull Long albumId) {
         this.albumId = albumId;
     }
 
+    @NonNull
     public Boolean getIs_liked() {
         return is_liked;
     }
 
-    public void setIs_liked(Boolean is_liked) {
+    public void setIs_liked(@NonNull Boolean is_liked) {
         this.is_liked = is_liked;
     }
 
+    @NonNull
     public Boolean getIs_deleted() {
         return is_deleted;
     }
 
-    public void setIs_deleted(Boolean is_deleted) {
+    public void setIs_deleted(@NonNull Boolean is_deleted) {
         this.is_deleted = is_deleted;
     }
 
+    @NonNull
     public LocalDateTime getCreated() {
         return created;
     }
 
-    public void setCreated(LocalDateTime created) {
+    public void setCreated(@NonNull LocalDateTime created) {
         this.created = created;
     }
 
     @Override
     public String toString() {
         return "Photo{" +
-                "photo_id=" + photoId +
+                "photoId=" + photoId +
                 ", name='" + name + '\'' +
                 ", path='" + path + '\'' +
-                ", album_id=" + albumId +
+                ", albumId=" + albumId +
                 ", is_liked=" + is_liked +
                 ", is_deleted=" + is_deleted +
-                ", created=" + created +
+               ", created=" + created +
                 '}';
     }
 }
