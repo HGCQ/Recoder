429bbcaa4f3390988082e52cc42f3cb54fcfc0a6
Mangjun <myoungjun55@naver.com>
2024-09-20 21:59:35 +0900

    [+] 채팅 관련 액티비티 및 기능

---
+++ client/app/src/main/java/yuhan/hgcq/client/config/ChatWebSocketClient.java
@@ -0,0 +1,74 @@
+package yuhan.hgcq.client.config;
+
+import android.util.Log;
+
+import com.google.gson.Gson;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.WebSocket;
+import okhttp3.WebSocketListener;
+import yuhan.hgcq.client.model.dto.chat.ChatDTO;
+
+public class ChatWebSocketClient extends WebSocketListener {
+
+    private WebSocket webSocket;
+    private final Gson gson = new Gson();
+    private final static String SERVER_IP = "127.0.0.1";
+    private final ChatWebSocketCallback callback;
+    private Long albumId;
+
+    public interface ChatWebSocketCallback {
+        void onMessageReceived(ChatDTO chatDTO);
+    }
+
+    public ChatWebSocketClient(Long albumId, ChatWebSocketCallback callback) {
+        this.albumId = albumId;
+        this.callback = callback;
+    }
+
+    public void start() {
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("ws://" + SERVER_IP + ":8080/chat/" + albumId)
+                .build();
+
+        webSocket = client.newWebSocket(request, this);
+        client.dispatcher().executorService().shutdown();
+    }
+
+    @Override
+    public void onOpen(WebSocket webSocket, okhttp3.Response response) {
+        Log.i("WebSocket", "Connected");
+    }
+
+    @Override
+    public void onMessage(WebSocket webSocket, String text) {
+        Log.i("WebSocket", "Received message: " + text);
+
+        ChatDTO chatDTO = gson.fromJson(text, ChatDTO.class);
+        if (callback != null) {
+            callback.onMessageReceived(chatDTO);
+        }
+    }
+
+    @Override
+    public void onClosing(WebSocket webSocket, int code, String reason) {
+        webSocket.close(1000, null);
+        Log.i("WebSocket", "Closing: " + reason);
+    }
+
+    @Override
+    public void onFailure(WebSocket webSocket, Throwable t, okhttp3.Response response) {
+        Log.e("WebSocket Error", t.getMessage());
+    }
+
+    public void sendMessage(ChatDTO chatDTO) {
+        if (webSocket != null) {
+            String chatDTOJson = gson.toJson(chatDTO);
+            webSocket.send(chatDTOJson);
+            Log.i("WebSocket", "Message : " + chatDTO.getMessage());
+        }
+    }
+}
--- client/app/src/main/java/yuhan/hgcq/client/view/Chat.java
+++ client/app/src/main/java/yuhan/hgcq/client/view/Chat.java
@@ -7,11 +7,14 @@ import android.graphics.Rect;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -20,23 +23,43 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import yuhan.hgcq.client.R;
+import yuhan.hgcq.client.adapter.ChatAdapter;
+import yuhan.hgcq.client.config.ChatWebSocketClient;
+import yuhan.hgcq.client.controller.ChatController;
+import yuhan.hgcq.client.model.dto.album.AlbumDTO;
+import yuhan.hgcq.client.model.dto.chat.ChatDTO;
+import yuhan.hgcq.client.model.dto.member.MemberDTO;
+import yuhan.hgcq.client.model.dto.team.TeamDTO;
 
 public class Chat extends AppCompatActivity {
 
     /* View */
+    EditText chatting;
+    ImageButton arrow;
+    RecyclerView chatListView;
+    BottomNavigationView navi;
 
-    /* 개인, 공유 확인 */
+    /* Adapter */
+    ChatAdapter ca;
+
+    /* 받아올 값 */
+    MemberDTO loginMember;
+    TeamDTO teamDTO;
+    AlbumDTO albumDTO;
 
     /* 서버와 통신 */
-
-    /* 로컬 DB */
-
-    /* Toast */
-    Handler handler = new Handler(Looper.getMainLooper());
-
-    /* Request Code */
+    ChatController cc;
+    ChatWebSocketClient webSocket;
 
     /* 뒤로 가기 */
     @Override
@@ -44,6 +67,9 @@ public class Chat extends AppCompatActivity {
         switch (item.getItemId()) {
             case android.R.id.home:
                 Intent galleryPage = new Intent(this, Gallery.class);
+                galleryPage.putExtra("loginMember", loginMember);
+                galleryPage.putExtra("teamDTO", teamDTO);
+                galleryPage.putExtra("albumDTO", albumDTO);
                 startActivity(galleryPage);
                 finish();
                 return true;
@@ -54,9 +80,9 @@ public class Chat extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        getSupportActionBar().setTitle("앨범 생성");
+        getSupportActionBar().setTitle("채팅");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        super.onCreate(savedInstanceState);
 
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_chat);
@@ -68,20 +94,114 @@ public class Chat extends AppCompatActivity {
         });
 
         /* 서버와 연결할 Controller 생성 */
-
-        /* 로컬 DB 연결할 Repository 생성 */
+        cc = new ChatController(this);
 
         /* View와 Layout 연결 */
+        chatting = findViewById(R.id.chatting);
+
+        arrow = findViewById(R.id.arrow);
+
+        chatListView = findViewById(R.id.friendList);
+
+        navi = findViewById(R.id.bottom_navigation_view);
 
         /* 관련된 페이지 */
+        Intent groupMainPage = new Intent(this, GroupMain.class);
+        Intent friendListPage = new Intent(this, FriendList.class);
+        Intent likePage = new Intent(this, Like.class);
 
-        /* 개인, 공유 확인 */
-
+        Intent getIntent = getIntent();
         /* 받아 올 값 */
+        albumDTO = (AlbumDTO) getIntent.getSerializableExtra("albumDTO");
+        teamDTO = (TeamDTO) getIntent.getSerializableExtra("teamDTO");
+        loginMember = (MemberDTO) getIntent.getSerializableExtra("loginMember");
 
-        /* 개인 초기 설정 */
+        /* 초기 설정 */
+        cc.chatList(albumDTO.getAlbumId(), new Callback<List<ChatDTO>>() {
+            @Override
+            public void onResponse(Call<List<ChatDTO>> call, Response<List<ChatDTO>> response) {
+                if (response.isSuccessful()) {
+                    List<ChatDTO> chatList = response.body();
+                    ca = new ChatAdapter(loginMember, chatList);
+                    chatListView.setAdapter(ca);
+                    chatListView.scrollToPosition(ca.getItemCount() - 1);
+                    Log.i("Found ChattingList", "Success");
+                } else {
+                    Log.i("Found ChattingList", "Fail");
+                }
+            }
 
-        /* 공유 초기 설정 */
+            @Override
+            public void onFailure(Call<List<ChatDTO>> call, Throwable t) {
+                Log.e("Found ChattingList Error", t.getMessage());
+            }
+        });
+
+        /* WebSocket 초기화 */
+        if (albumDTO != null) {
+            webSocket = new ChatWebSocketClient(albumDTO.getAlbumId(), new ChatWebSocketClient.ChatWebSocketCallback() {
+                @Override
+                public void onMessageReceived(ChatDTO chatDTO) {
+                    Log.i("WebSocket", "Received message: " + chatDTO.getMessage());
+                    runOnUiThread(() -> {
+                        ca.addChat(chatDTO);
+                        chatListView.smoothScrollToPosition(ca.getItemCount() - 1);
+                    });
+                }
+            });
+
+            webSocket.start();
+        }
+
+        arrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = chatting.getText().toString();
+
+                if (!message.isEmpty()) {
+                    ChatDTO chatDTO = new ChatDTO();
+                    chatDTO.setAlbumId(albumDTO.getAlbumId());
+                    chatDTO.setWriterId(loginMember.getMemberId());
+                    chatDTO.setWriterName(loginMember.getName());
+                    chatDTO.setMessage(message);
+
+                    webSocket.sendMessage(chatDTO);
+                    ca.addChat(chatDTO);
+                    chatListView.smoothScrollToPosition(ca.getItemCount() - 1);
+                    chatting.setText(""); // 입력 필드 비우기
+                }
+            }
+        });
+
+        /* 내비게이션 바 */
+        navi.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int itemId = menuItem.getItemId();
+                if (itemId == R.id.fragment_home) {
+                    groupMainPage.putExtra("loginMember", loginMember);
+                    startActivity(groupMainPage);
+
+                    return true;
+                } else if (itemId == R.id.fragment_friend) {
+                    if (loginMember == null) {
+                        Toast.makeText(Chat.this, "로그인 후 이용 가능합니다.", Toast.LENGTH_SHORT).show();
+                    } else {
+                        friendListPage.putExtra("loginMember", loginMember);
+                        startActivity(friendListPage);
+                    }
+                    return true;
+                } else if (itemId == R.id.fragment_like) {
+                    likePage.putExtra("loginMember", loginMember);
+                    startActivity(likePage);
+
+                    return true;
+                } else if (itemId == R.id.fragment_setting) {
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 
     /* Confirm 창 */
