8a6513b1a6946aa8c1dacc174a9c5c2be800dc2b
Mangjun <myoungjun55@naver.com>
2024-09-23 13:55:15 +0900

    [*] 갤러리 액티비티 개인 모드 추가

---
+++ client/app/src/main/java/yuhan/hgcq/client/adapter/PrivateGalleryPhotoAdapter.java
@@ -0,0 +1,80 @@
+package yuhan.hgcq.client.adapter;
+
+import android.content.Context;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+import yuhan.hgcq.client.R;
+import yuhan.hgcq.client.model.dto.photo.PhotoDTO;
+
+public class PrivateGalleryPhotoAdapter extends RecyclerView.Adapter<PrivateGalleryPhotoAdapter.PrivateGalleryPhotoViewHolder> {
+
+    private List<PhotoDTO> photoList;
+    private OnItemClickListener listener;
+    private Context context;
+
+    public PrivateGalleryPhotoAdapter(List<PhotoDTO> photoList, Context context) {
+        this.photoList = photoList;
+        this.context = context;
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(View view, int position);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        this.listener = listener;
+    }
+
+    public static class PrivateGalleryPhotoViewHolder extends RecyclerView.ViewHolder {
+        public ImageView photo;
+
+        public PrivateGalleryPhotoViewHolder(@NonNull View view, OnItemClickListener listener) {
+            super(view);
+
+            photo = view.findViewById(R.id.photo);
+
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (listener != null) {
+                        int position = getAdapterPosition();
+                        if (position != RecyclerView.NO_POSITION) {
+                            listener.onItemClick(v, position);
+                        }
+                    }
+                }
+            });
+        }
+    }
+
+    @NonNull
+    @Override
+    public PrivateGalleryPhotoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View photoView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_photo_image, parent, false);
+        return new PrivateGalleryPhotoViewHolder(photoView, listener);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PrivateGalleryPhotoViewHolder holder, int position) {
+        String path = photoList.get(position).getPath();
+        Glide.with(context)
+                .load(Uri.parse(path))
+                .into(holder.photo);
+    }
+
+    @Override
+    public int getItemCount() {
+        return photoList.size();
+    }
+}
